7b1fdcebd5b05f8c311ca090577b18eaf1c575bb
aramind <r.m.miranda.ece@gmail.com>
2023-11-29 18:47:08 +0800

    feat: adding update task route and controller

--- backend/tasks/controllers/task.js
+++ backend/tasks/controllers/task.js
@@ -3,6 +3,7 @@ const handleError = require("../helpers/errorCatcher");
 
 const taskController = {
   // controller | GET | tasks/:userId | desc: Get all tasks for a specific user | req: {userId} | res: List of tasks for a specific user
+  // localhost:4000/tasks/1234
   getTaskByUserId: async (req, res) => {
     // console.log(req.params);
     try {
@@ -29,6 +30,7 @@ const taskController = {
   // TODO: controller | GET | tasks/:type?date_to=date&date_from=date | desc: Get all tasks with a specific type | req: {task type, date_to, date_from} | res: List of all tasks with a specific type
 
   // controller | POST | tasks | desc: create a task | req: {task type, userId, metadata} | res: created task
+  // localhost:4000/tasks
   addTask: async (req, res) => {
     console.log("addTask controller reached");
     try {
@@ -59,7 +61,7 @@ const taskController = {
         type,
         userId: userId.toString(), // question for reviewer: do I need to include pa this conditional ?
         label,
-        Metadata: typeof Metadata === "object" ? Metadata : { Metadata }, //question for reviewer: do we need to include pa this conditional?
+        Metadata: typeof Metadata === "object" ? Metadata : { Metadata }, //question for reviewer: do I need to include pa this conditional?
       });
 
       const createdTask = await newTask.save();
@@ -73,7 +75,57 @@ const taskController = {
       handleError(res, error);
     }
   },
-  // TODO: controller | PUT | tasks | desc: updates a task | req: {updated info} | res: updated task
+  //controller | PUT | tasks | desc: updates a task | req: {updated info} | res: updated task
+  // localhost:4000/tasks
+  // NOTE for reviewer: why not do it by adding the taskId via params to determine which existing (if there is one) task to update? is it better to include the taskId in the req.body? :)
+  updateTask: async (req, res) => {
+    console.log("UPDATING TASK");
+    try {
+      // const { taskId } = req.params;
+
+      const { taskId, type, label, userId, Metadata } = req.body;
+
+      // check for fields
+      if (!taskId || (!type && !label && !userId && !Metadata)) {
+        return res.status(400).json({
+          success: false,
+          message:
+            "Invalid Request. Provide a valid task ID and at least one field to update.",
+          data: null,
+        });
+      }
+
+      // Find the task by id
+      const existingTask = await Task.findById(taskId);
+
+      if (!existingTask) {
+        return res.status(404).json({
+          success: false,
+          message: "Task not found",
+          data: null,
+        });
+      }
+
+      // updates the task
+      if (type) existingTask.type = type;
+      if (label) existingTask.label = label;
+      if (userId) existingTask.userId = userId.toString();
+      if (Metadata)
+        existingTask.Metadata =
+          typeof Metadata === "object" ? Metadata : { Metadata };
+
+      // saving the updated task
+      const updatedTask = await existingTask.save();
+
+      res.status(200).json({
+        success: true,
+        message: "Task updated successfully",
+        data: updatedTask,
+      });
+    } catch (error) {
+      handleError(res, error);
+    }
+  },
 };
 
 module.exports = taskController;
--- backend/tasks/routes/taskRouter.js
+++ backend/tasks/routes/taskRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/task");
 
-console.log("taskrouter reached");
+console.log("task router reached");
 
 router.get("/:userId", taskController.getTaskByUserId);
 router.post("/", taskController.addTask);
+router.put("/", taskController.updateTask);
 
 module.exports = router;
